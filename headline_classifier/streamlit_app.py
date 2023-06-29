69e43f860d1a28086762138d74f561b67b7ab305
Saul Lee <saul.s.lee@gmail.com>
2023-06-29 13:54:43 -0400

    handle more than one model

--- headline_classifier/streamlit_app.py
+++ headline_classifier/streamlit_app.py
@@ -2,13 +2,20 @@
 import spacy
 import streamlit as st
 
+
+def get_category(nlp, model_name, title):
+    doc = nlp(title)
+    category = max(doc.cats, key=doc.cats.get)
+    score = max(doc.cats.values())
+    st.write(model_name)
+    st.write("predicted category: ", category)
+    st.write("predicted score: ", score)
+
+
 # this app will use pre-trained classifiers to categorize
 # input headline
 
 st.markdown("# Headline Title Classifier")
-
-nlp = spacy.load("models/model-best")
-
 st.markdown(
     """
     This app will categorize a headline title using a pre-trained
@@ -16,14 +23,16 @@ st.markdown(
     """
 )
 
+# load models
+nlp = spacy.load("models/spacy_base/textcat_model_2023-06-25/model-best")
+nlp_distilbert = spacy.load("models/distilbert/textcat_model_transformer_2023-06-23/model-best")
+
+
 # text input section and seed with default headline
 title = st.text_input("Headline", "Men Walk on Moon")
 
 if st.button("Run"):
-    doc = nlp(title)
-    category = max(doc.cats, key=doc.cats.get)
-    score = max(doc.cats.values())
-    st.write("predicted category: ", category)
-    st.write("predicted score: ", score)
+    get_category(nlp, "spacy_base", title)
+    get_category(nlp_distilbert, "distilbert", title)
 else:
     st.markdown("__Please click Run to generate predictions__")
