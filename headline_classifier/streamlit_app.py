1ff90d1e07d4d3dc8f0b63fd8e44eb672a3f4d00
Saul Lee <saul.s.lee@gmail.com>
2023-07-06 10:43:11 -0400

    add logic to load model from s3

--- headline_classifier/streamlit_app.py
+++ headline_classifier/streamlit_app.py
@@ -1,6 +1,8 @@
 
 import spacy
 import streamlit as st
+import s3fs
+import os
 
 
 def get_category(nlp, model_name, title):
@@ -10,6 +12,64 @@ def get_category(nlp, model_name, title):
     st.write(model_name)
     st.write("predicted category: ", category)
     st.write("predicted score: ", score)
+
+
+@st.cache_resource
+def load_model_from_s3(
+    s3_model_key,
+    model_type,
+    save_basepath,
+    save_filename,
+    model_path="model-best",
+):
+
+    if not os.path.exists(save_basepath):
+        os.makedirs(save_basepath)
+
+    save_path = os.path.join(save_basepath, save_filename)
+
+    if not os.path.exists(save_path):
+        with st.spinner(
+            f"""Loading NLP ({model_type}) models...
+            (note this could take a few min as models can be large)
+            """
+        ):
+            fs = s3fs.S3FileSystem(anon=False)
+            fs.get_file(s3_model_key, save_path)
+
+    save_model_path = os.path.join(save_basepath, model_path)
+
+    if not os.path.exists(save_model_path):
+        import shutil
+
+        shutil.unpack_archive(save_path, save_model_path)
+
+    model = spacy.load(save_model_path)
+
+    return model
+
+
+@st.cache_resource
+def load_local_model(model_path, model_type):
+
+    with st.spinner(
+        f"""Loading NLP ({model_type}) models...
+        (note this could take a few min as models can be large)
+        """
+    ):
+        model = spacy.load(model_path)
+    st.success("Done loading models!")
+
+    return model
+
+
+s3_bucket = st.secrets["S3_BUCKET"]
+s3_model_path = "prod/streamlit/headline_classifier/models/spacy_base/textcat_model_2023-06-25/model-best.zip"
+
+s3_key = os.path.join(s3_bucket, s3_model_path)
+
+save_basepath = "models/spacy_base"
+save_filename = "model-best.zip"
 
 
 # this app will use pre-trained classifiers to categorize
@@ -24,16 +84,21 @@ st.markdown(
 )
 
 # load models
-with st.spinner("Loading NLP models... (note this could take a few min as model can be large)"):
-    nlp = spacy.load("models/spacy_base/textcat_model_2023-06-25/model-best")
-    nlp_distilbert = spacy.load("models/distilbert/textcat_model_transformer_2023-06-23/model-best")
-st.success('Done loading models!')
+
+# nlp = spacy.load("models/spacy_base/textcat_model_2023-06-25/model-best")
+nlp_distilbert = load_local_model(
+    "models/distilbert/textcat_model_transformer_2023-06-23/model-best",
+    "distilbert"
+)
+
+nlp = load_model_from_s3(s3_key, "spacy_base", save_basepath, save_filename)
 
 
 # text input section and seed with default headline
-title = st.text_input("Headline", "Men Walk on Moon")
+title = st.text_input("Headline", "Man Walks on Moon")
 
 if st.button("Run"):
+    st.markdown(f"__Category predictions for phrase: {title}__")
     get_category(nlp, "spacy_base", title)
     get_category(nlp_distilbert, "distilbert", title)
 else:
